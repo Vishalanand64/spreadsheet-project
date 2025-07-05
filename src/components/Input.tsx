63390cc8d0a34411d525732fdec0599be5918f63
user.name : "Vishal Anand <user.email : "vishalanand0064@gmail.com>
2025-07-05 19:10:56 +0530

    overflow handled

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,8 +3,8 @@ import  { useState } from 'react'
 const Input = (props : any) => {
     const [input , setInput] = useState("")
   return (
-    <div className='h-full border-r-2 border-[#eeeeee] flex items-center px-1' style={{width:props.width}}>
-        <input type="text" value={input} onChange={(e) => setInput(e.target.value)} className='outline-none'  />
+    <div className='h-full border-r-2 border-[#eeeeee] flex items-center px-1 overflow-hidden' style={{width:props.width}}>
+        <input type="text" value={input} onChange={(e) => setInput(e.target.value)} className='outline-none overflow-hidden'  />
     </div>
   )
 }
